3ca3a2fbabbcad76f3f0255a1ebe9e042cb2c8c8
cironis <66842583+cironis@users.noreply.github.com>
2020-06-12 14:45:03 -0300

    Update quase_primo.py

--- quase_primo.py
+++ quase_primo.py
@@ -1,9 +1,17 @@
+linha1 = input().split()
+linha2 = input().split()
+
+
+n = int(linha1[0])
+s = linha2
+
+
 def powerset(s):
     
     x = len(s)
     multipliers = {
-        "subtract_multipliers" : [],
-        "add_multipliers" : []
+        "subtract" : [],
+        "add" : []
     }     
     
     
@@ -14,21 +22,30 @@ def powerset(s):
     
         for j in range(x):
             if i & (1 << j):
-                single_set.append(s[j])
+                single_set.append(int(s[j]))
     
         for num in single_set:
             multiply *= num
         
         if len(single_set) == 1:
-            multipliers["add_multipliers"].append(single_set[0])
+            multipliers["add"].append(single_set[0])
             
         elif len(single_set) > 1:
-            multipliers["subtract_multipliers"].append(multiply)
+            multipliers["subtract"].append(multiply)
     
     return multipliers
 
+multiplicadores = powerset(s)
 
+value_to_add = 0
+value_to_subtract = 0
 
-
-
-powerset([4,5,6])
+for num in multiplicadores["add"]:
+    if num <= n:
+        value_to_add += n//num
+        
+for num in multiplicadores["subtract"]:
+    if num <= n:
+        value_to_subtract += n//num
+    
+return n - value_to_add + value_to_subtract
