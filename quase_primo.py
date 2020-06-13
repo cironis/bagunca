219d33804109361c10f85b47dea0edb420688bf0
cironis <66842583+cironis@users.noreply.github.com>
2020-06-13 13:16:19 -0300

    Update quase_primo.py

--- quase_primo.py
+++ quase_primo.py
@@ -3,51 +3,13 @@ linha2 = input().split()
 
 
 n = int(linha1[0])
-s = linha2
+lista = linha2
 
+all_multiples_set = set()
 
-def powerset(s):
+for i in range(len(lista)):
+    interador = int(linha2[i])
+    one_multiple_set = set(range(interador,n if n % interador != 0 else n + interador,interador))
+    all_multiples_set = all_multiples_set.union(one_multiple_set)
     
-    x = len(s)
-    multipliers = {
-        "subtract" : [],
-        "add" : []
-    }     
-    
-    
-    for i in range(1 << x):
-    
-        single_set = []
-        multiply = 1
-    
-        for j in range(x):
-            if i & (1 << j):
-                single_set.append(int(s[j]))
-    
-        for num in single_set:
-            multiply *= num
-            if multiply > n:
-                break
-        
-        if len(single_set) == 1:
-            multipliers["add"].append(single_set[0])
-            
-        elif len(single_set) > 1:
-            multipliers["subtract"].append(multiply)
-    
-    return multipliers
-
-multiplicadores = powerset(s)
-
-value_to_add = 0
-value_to_subtract = 0
-
-for num in multiplicadores["add"]:
-    if num <= n:
-        value_to_add += n//num
-        
-for num in multiplicadores["subtract"]:
-    if num <= n:
-        value_to_subtract += n//num
-    
-print(n - value_to_add + value_to_subtract)
+print n - len(all_multiples_set)
