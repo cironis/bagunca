d42838800fad795fd21808494863aba9adcf88ab
cironis <66842583+cironis@users.noreply.github.com>
2020-06-13 16:29:56 -0300

    Update quase_primo.py

--- quase_primo.py
+++ quase_primo.py
@@ -6,48 +6,31 @@ n = int(linha1[0])
 s = linha2
 
 
-def powerset(s):
-    
-    x = len(s)
-    multipliers = {
-        "subtract" : [],
-        "add" : []
-    }     
-    
-    
-    for i in range(1 << x):
-    
-        single_set = []
-        multiply = 1
-    
-        for j in range(x):
-            if i & (1 << j):
-                single_set.append(int(s[j]))
-    
-        for num in single_set:
-            multiply *= num
-            if multiply > n:
-                break
-        
-        if len(single_set) == 1:
-            multipliers["add"].append(single_set[0])
-            
-        elif len(single_set) > 1:
-            multipliers["subtract"].append(multiply)
-    
-    return multipliers
 
-multiplicadores = powerset(s)
+x = len(s)
 
 value_to_add = 0
 value_to_subtract = 0
 
-for num in multiplicadores["add"]:
-    if num <= n:
-        value_to_add += n//num
-        
-for num in multiplicadores["subtract"]:
-    if num <= n:
-        value_to_subtract += n//num
-    
+
+for i in range(1 << x):
+
+    single_set = []
+    multiply = 1
+
+    for j in range(x):
+        if i & (1 << j):
+            single_set.append(int(s[j]))
+
+    for num in single_set:
+        multiply *= num
+        if multiply > n:
+            break
+
+    if len(single_set) == 1:
+        value_to_add += n//single_set[0]
+
+    elif len(single_set) > 1:
+        value_to_subtract += n//multiply
+
 print(n - value_to_add + value_to_subtract)
